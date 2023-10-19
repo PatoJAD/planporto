a08599c67851452a539c68b8d140691b00de815d
Joaquin (Pato) Decima <jdecima@vasak.net.ar>
2023-10-19 12:59:26 -0300

    clean js

--- js/custom.js
+++ js/custom.js
@@ -7,14 +7,6 @@
       $('.preloader').fadeOut(1000); // set duration in brackets    
     });
 
-
-    // MENU
-    $('.menu-burger').on('click', function() {
-      $('.menu-bg, .menu-items, .menu-burger').toggleClass('fs');
-      $('.menu-burger').text() == "☰" ? $('.menu-burger').text('✕') : $('.menu-burger').text('☰');
-    });
-
-
     // ABOUT SLIDER
     $('body').vegas({
         slides: [
